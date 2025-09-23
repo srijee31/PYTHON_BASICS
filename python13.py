fb5e68e748ff5e93d00758726d2f0eede2986b24
Srijeeth KS <srijeethkssrijeeth@gmail.com>
2025-09-23 14:41:43 +0530

    Add files via upload

---
+++ python13.py
@@ -0,0 +1,36 @@
+# set={"Hello","World","Python"}
+# b= set.add("Raju")
+# c= set.discard("Raj")
+# print(set)
+
+
+# list1 = [1,2,3,4,5]
+# list2 = [6,7,8,9,10]
+
+
+# list1.extend(list2)
+# print(list1)
+# print(list2)      
+
+
+# a=2
+# while a<=10:
+#     print(a)
+#     a+=2
+
+# import time
+# a= 10
+# while a>0:
+#     print(f"The time remanining now is {a} seconds")
+#     time.sleep(1)
+#     a= a-1
+
+def addition():
+     numbers = [10, 20, 30, 40, 60]
+     total = 0
+
+     for num in numbers:
+         total += num
+
+     print("Total sum:", total)
+addition()   
---
+++ python14.py
@@ -0,0 +1,65 @@
+# functions.....
+
+#basic examaple of function
+
+# def marriage(boy, girl):
+#     print(f"Boy namne is {boy}")
+#     print(f"Gilr name is {girl}")
+#     print(f"{boy} and {girl} are married now.")
+#     return
+# marriage("Rohit", "Ankita")#positional argument
+# marriage("Rvai", "Sonal") #positional argument
+
+#////////////////////////////////////////       
+#multiplication table using function
+
+# def tables(num):
+#      for i in range (1,11):
+      
+#             print(f"{num} X {i} = {num*i}")
+# tables(2)  
+# tables(3)
+# tables(4)
+# tables(5)
+# tables(6)
+# tables(7)
+# tables(8)
+# tables(9)
+# tables(10)
+
+#////////////////////////////////////////       
+#adding two numbers using function
+
+# def add_numbers(a, b):
+#     return a - b
+
+# result = add_numbers(10, 20)
+# print("The sum is:", result)
+                    
+#////////////////////////////////////////                    
+#homework
+
+# def greet():
+#     name= input("Enter  your name: ")
+#     print(f"Hello, {name}, welcome to the world of Python programming!")
+# greet()   
+
+
+#//////////////////////////////////////////                 
+# def my_name(name):
+#     print(f"Welcome {name.upper()}")
+   
+
+# my_name("srijee")  
+
+#///////////////////////////////////////////
+
+def number(a, b):
+     return(a+b)
+print(number(20,10)) 
+
+
+
+
+  
+                       
